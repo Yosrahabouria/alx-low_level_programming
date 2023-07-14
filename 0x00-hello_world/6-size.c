bda1a3c9fba697c82ef1ccf2ee934d87cca9fb6c
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-14 09:02:44 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-printf("size of a char: %lu byte(s)", sizeof(char)\n);
+printf("size of a char: %lu byte(s)\n", sizeof(char));
 printf("size of int: %lu byte(s)\n", sizeof(int));
 printf("size of long int: %lu byte(s)\n", sizeof(long int));
 printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
