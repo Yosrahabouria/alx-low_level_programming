5488bb58ff029890faf75e8a2790a91ae3fe9b7a
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-13 18:04:34 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 1 #include <stdio.h>
 2 int main(void)
-3 { 
-4       puts ("\"Programming is like building a multilingual puzzle");
-5	return 0;
-6 } 
+3{
+4 puts("\"Programming is like building a multilingual puzzle");
+5 return (0);
+6}
