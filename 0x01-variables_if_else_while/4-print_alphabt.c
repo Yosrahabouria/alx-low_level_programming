04950cd0b6c4a9ae5a7a5262445309f7de1017bd
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-15 08:04:09 -0700

    a

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,13 +4,15 @@
  * Return:always 0 (success)
 */
 int main(void)
+
 {
 int n = 97;
 
 while (n <= 122)
 {
-	if (n == 101 || n == 113)
+if (n == 101 || n == 113)
 		{
+n++;
 		continue;
 		putchar(n);
 			n++;
