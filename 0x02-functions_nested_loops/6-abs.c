5db4920657c4ceb6584cf7a4dd00a42ad98ba505
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-18 10:41:47 -0700

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - check if alphabet uppercase or lower
+ * Return:always 0 (success)
+ * @c : the character ASCII code
+*/
+int _abs(int)
+{
+	if (a >= 0)
+{ 
+		return (i);
+}
+	else 
+	{
+		return (i * -1);
+	}
+_putchar('\n');
+}
