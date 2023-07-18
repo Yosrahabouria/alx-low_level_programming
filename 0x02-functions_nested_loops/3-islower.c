95f5c0da2b10b13d3b4bae84fe8de60c81e690a2
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-18 07:37:34 -0700

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _islower - print alphabet in lower case
- * Return:always 0 or 1 (success)
+ * Return: 1 for lowaer cases character. 0 for the rest (success)
+ * @c : the character in ASCII code
 */
 int _islower(int c)
 {
@@ -9,8 +10,7 @@ if (c >= 97 && c <= 122)
 {
 return (1);
 }
-{
-else return (0);
-}
+else
+{return (0); }
 _putchar('\n');
 }
