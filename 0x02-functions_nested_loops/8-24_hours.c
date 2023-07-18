21c6bf7ee6ed96c75d37b828b823ff6d324e1007
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-18 12:48:47 -0700

    a

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,7 @@
-
 #include "main.h"
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * Return:integer (success)
- * @c : print integer
- * @a : print integer
- * @b : print integer
- * @d : print  integer
+ * Return:no return (success)
 */
 
 void jack_bauer(void)
@@ -20,7 +15,7 @@ void jack_bauer(void)
 		for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
-			
+				{
 					if (a >= 2 && b >= 4)
 						break;
 					_putchar(a + 48);
@@ -29,8 +24,9 @@ void jack_bauer(void)
 					_putchar(a + 48);
 					_putchar(c + 48);
 					_putchar(d + 48);
-                                        _putchar('\n');
+					_putchar('\n');
+
+				}
+			}	}
 }
-}
-}  
 }
