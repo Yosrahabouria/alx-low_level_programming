8bf321f390e9de51a82d04bcc51aab202b84f7be
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-20 16:50:31 -0700

    a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers from 0 to 14 10 times
+ * Return: result (success)
+*/
+void more_numbers(void)
+{ int i;
+int j;
+
+for (j = 1; j <= 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
