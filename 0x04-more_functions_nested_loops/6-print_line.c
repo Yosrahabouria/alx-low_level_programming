444f7e128fd75573fe19cb47d0639a1473fbc926
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-20 17:41:27 -0700

    a

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,15 +11,15 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar('_');
+			putchar('_');
 		}
-		_putchar('\n');
+		putchar('\n');
 }
-return (0);
+
 }
