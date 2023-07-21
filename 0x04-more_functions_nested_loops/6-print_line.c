3dbff57aabcf1ec0de0aecb374ba36e6da08e717
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-20 17:29:42 -0700

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+/**
+ * print_line- print alphabet in lower case
+ * Return:lines (success)
+ * @n : input number
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
