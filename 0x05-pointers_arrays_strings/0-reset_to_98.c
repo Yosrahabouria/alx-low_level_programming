9a124054dc679af6c241a005782aaeafab3cedcb
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-24 05:02:16 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * Return:always 0 (success)
+ * @n : the input number
+*/
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = &n;
+	putchar(*p);
+
+		return (0);
+}		
