934090b96759edbb646f2a06473d846a78a68148
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-24 08:59:16 -0700

    a

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * print_rev - print string in reverse
+ * print_rev- print string in reverse
  * Return:always 0 (success)
  * @s : the input char
 */
 void print_rev(char *s)
 {
 int a, b, c;
+
 a = 0;
 
 while (s[a] != '0')
@@ -17,6 +18,7 @@ while (s[a] != '0')
 	a++;
 }
 c = a;
+
 for (b = c - 1; b >= 0; b--)
 {
 	putchar(s[b]);
