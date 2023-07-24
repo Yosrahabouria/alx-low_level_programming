0d5f1359fc50ecf4d6d415aefac4d8d94568e85f
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-24 05:39:58 -0700

    a

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int _strlen - return length of string
+ * _strlen- return length of string
  * Return:always 0 (success)
  * @s : the input string
 */
- int _strlen(char *s)
+int _strlen(char *s)
 
 {
 	int len;
-	
+
 	len = strlen(*s);
 
 }
