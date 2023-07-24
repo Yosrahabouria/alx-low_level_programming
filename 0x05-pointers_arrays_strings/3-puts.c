9aa9d59e07c17c2280f39bf113758fb932be01cf
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-24 06:24:55 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+stdout
+#include "main.h"
+/**
+ * _puts- print alphabet in lower case
+ * Return:always 0 (success)
+ *  * @str : the input string
+*/
+void _puts(char *str)
+{
+	puts(str);
+	return ('\n');
+}
