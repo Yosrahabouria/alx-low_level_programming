594369f269c204f9b58f5a66b13c9a37c3186eab
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-26 10:56:14 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 	int j;
 
 	j = 0;
-while ((s1[j] != '\0') && (s2[j] != '\0'))
+while (s1[j] != '\0' && (s2[j] != '\0')
 	{
 		if (s1[j] != s2[j])
 		{
@@ -21,5 +21,5 @@ while ((s1[j] != '\0') && (s2[j] != '\0'))
 		}
 		j++;
 	}
-		return ('\0');
+		return (0);
 }
