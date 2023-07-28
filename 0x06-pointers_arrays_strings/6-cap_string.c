3e79b9fbc6a9cac6ed3930972e9645760fd232a5
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-27 18:34:40 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,14 +11,12 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (str[++index])
+	while (str[index])
 	{
-		while (!(str[index] >= 97) && (str[index] <= 122))
-		
+		while (!(str[index] >= 'a') && (str[index] <= 'z'))
+		index++;
 
-
-		if (str[index - 1] == ' ' ||
-		   str[index - 1] == '\t' ||
+if (str[index - 1] == ' ' || str[index - 1] == '\t' ||
 		   str[index - 1] == '\n' ||
 		   str[index - 1] == ',' ||
 		   str[index - 1] == ';' ||
@@ -29,12 +27,11 @@ char *cap_string(char *str)
 		   str[index - 1] == '(' ||
 		   str[index - 1] == ')' ||
 		   str[index - 1] == '{' ||
-		   str[index - 1] == '}')
+		   str[index - 1] == '}' ||
+		   index == 0)
 
 		   str[index] -= 32;
-		
 		index++;
-	
 	}
 return (str);
 }
