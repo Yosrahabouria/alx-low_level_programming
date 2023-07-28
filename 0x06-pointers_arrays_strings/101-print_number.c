3fe13e056010374fc8a1eddc76faa8c1d2069583
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-27 18:04:47 -0700

    a

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * print_number - print number chars
+ * @n: integer params
+ * Return: 0
+*/
+print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		putchar('-');
+		n1 = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	putchar((n1 % 10) + '0');
+}
