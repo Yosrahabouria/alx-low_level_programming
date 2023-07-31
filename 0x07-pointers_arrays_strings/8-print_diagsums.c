ac683553df87ec06f530c69dbb03c1e306e1a2a1
Yosrahabouria <yosrahabouria@gmail.com>
2023-07-31 15:39:48 -0700

    a

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_diagsums - Entry point
+ * @a : input
+ * @size : input
+ * Return: copied memory with n bytes changed
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, i;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 = sum1 + a[i * size + i];
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		sum2 += a[i * size + (size - i - 1)];
+	}
+	putchar(sum1);
+putchar(sum2);
+}
+
