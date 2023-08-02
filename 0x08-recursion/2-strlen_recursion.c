8f686430beb03c511f3e3df77ce1e56c647663bc
Yosrahabouria <yosrahabouria@gmail.com>
2023-08-02 10:48:05 -0700

    a

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,11 @@
 */
 int _strlen_recursion(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		return (i);
+if (*s)
+	{       i++;
+		i = i + _strlen_recursion(s + 1);
 	}
+return (i);
 }
