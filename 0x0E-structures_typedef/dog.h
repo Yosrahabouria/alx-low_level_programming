bbd8b4e6b949e10e56528f16cd0dcd44e3ebb10e
Yosrahabouria <yosrahabouria@gmail.com>
2023-08-14 10:09:46 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - a struct
+ * @d : pointer to a struct dog
+ * @name : name of the dog
+ * @age : age of the dog
+ * @owner : owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* MAIN_H */
