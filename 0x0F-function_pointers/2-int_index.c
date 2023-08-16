42e1651778e620117fcb6bed55cccadd8573efab
Yosrahabouria <yosrahabouria@gmail.com>
2023-08-16 15:24:52 -0700

    a

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "function_pointers.h"
+/**
+ * int_index - a function that copy memory area
+ * @size : input value
+ * @array : input value
+ * @cmp : input value
+ * Return: 0
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
