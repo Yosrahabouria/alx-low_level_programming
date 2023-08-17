44cbd116dd2f5fbe68f4dfa24f3c9821836cd1cd
Yosrahabouria <yosrahabouria@gmail.com>
2023-08-17 04:25:04 -0700

    a

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum-them_all a function that copy memory area
+ * @n : input value
+ * @... : input value
+ * Return: if n == 0-0
+ * otherwise-the sum of all parametrs
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
+}
