7af9027b0e193be9f3b8593ecae6dd04f18709d5
Yosrahabouria <yosrahabouria@gmail.com>
2023-08-28 12:08:19 -0700

    a

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * pop_listint - a function that delete the head
+ * of a node in the list
+ * @head : input value
+ * Return: if the linked list is empty return 0
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *k = *head;
+	int i = (*head)->n;
+
+	if (*head == NULL)
+	{
+		return(0);
+	}
+	*head = (*head)->next;
+	free(k);
+	return (i);
+}
