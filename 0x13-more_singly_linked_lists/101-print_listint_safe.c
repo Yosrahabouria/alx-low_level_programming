d2d2fcd8d9a17a76585409e2588a6f58fdafb9db
Yosrahabouria <yosrahabouria@gmail.com>
2023-08-29 07:52:57 -0700

    a

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * print_listint_safe - a function that copy memory area
+ * @head : input value
+ * Return: copied memory with n bytes changed
+*/
+size_t print_listint_safe(const listint_t *head)
