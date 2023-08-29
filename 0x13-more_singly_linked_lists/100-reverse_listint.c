801a0231ed413d89b94e01f62667ac02a1815544
Yosrahabouria <yosrahabouria@gmail.com>
2023-08-29 07:37:11 -0700

    a

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "main.h"
+#include "lists.h"
 /**
  * reverse_listint - a function that reverse a linked list
  * @head : input value
