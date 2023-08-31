bf1d4cd327e336d859ba02c3ffc45ab33f0f052c
Yosrahabouria <yosrahabouria@gmail.com>
2023-08-31 14:39:29 -0700

    a

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,6 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > 64)
 		return (-1);
 
-	*n = ((1UL << index) | *n);
+/*	*n = ((1UL << index) | *n);*/
+	*n |= (1 << index);
 	return (1);
 }
