a6881d442fd572b0070c4ed8cbf8a92be799ef99
Yosrahabouria <yosrahabouria@gmail.com>
2023-08-31 10:38:04 -0700

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,8 +20,10 @@ unsigned int binary_to_uint(const char *b)
 	for (a = 0; b[a]; a++)
 	{
 		if (b[a] < '0' || b[a] > '1')
-			return (0);
+		return (0);
+
 		j = 2 * j + (b[a] - '0');
 	}
+
 	return (j);
 }
