02dca44136f9e5e4dd8a5d939fd8385127f236d3
Yosrahabouria <yosrahabouria@gmail.com>
2023-09-04 12:57:49 -0700

    a

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _memcpy - a function that copy memory area
+ * @dest : memory where is stored
+ * @src : memory where is copied
+ * @n : number of bytes
+ * Return: copied memory with n bytes changed
+*/
+
