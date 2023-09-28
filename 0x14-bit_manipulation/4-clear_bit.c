08af6036541d33c25fd9f787821bc2c895bdff5f
Yosrahabouria <yosrahabouria@gmail.com>
2023-09-28 04:47:43 -0700

    a

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,8 +11,11 @@
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
+	if
+	(index > 64)
 	{
-		return (-1);
+return (-1);
+	   *n = (~(1UL << index) & *n);
+return (1);
 	}
 }
