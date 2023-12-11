d27bca8d79977bb20212e0f349151556fa82aa00
Yosrahabouria <yosrahabouria@gmail.com>
2023-12-11 10:53:42 -0800

    a

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -c -fPIC *.c -shared -o liball.so *.o
+gcc -Wall -Werror -Wextra -pedantic -c -fPIC *.c 
+gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
